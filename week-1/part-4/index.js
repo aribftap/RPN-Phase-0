58010a1523a87314b702aa6d7770bbf6f8687e01
Arib Fayyadhtamir Andika Putra <arib.ft07@gmail.com>
2024-08-05 20:37:07 +0700

    chore: Add Javascript learning materials (Part 4-6)

---
+++ week-1/part-4/index.js
@@ -0,0 +1,1 @@
+// just push to github
---
+++ week-1/part-5/conditional-statement.js
@@ -0,0 +1,16 @@
+// Comparison Operators
+let x = 5;
+
+// equal to aka sama dengan
+console.log(x == 5);
+console.log(x == 8);
+console.log(x == "5");
+
+// equal value and equal type aka nilai yang sama dan jenis yang sama
+console.log(x === 5);
+console.log(x === "5");
+
+// not equal aka tidak sama dengan
+console.log(x != 6);
+
+
